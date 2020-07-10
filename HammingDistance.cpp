9d232a7e38d1b1a523b52ef8ca5d541493f833d9
Utkarsh Kumar <31178132+Utkarsh9799@users.noreply.github.com>
2020-07-10 19:05:12 +0530

    Hamming Distance solution

---
+++ HammingDistance.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int ctr = 0;
+        int bits = x ^ y;
+        while(bits != 0)
+        {
+            if(bits & 1)
+                ctr++;
+            
+            bits = bits >> 1;
+        }
+        return ctr;
+    }
+};
