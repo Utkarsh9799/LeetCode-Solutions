86943f92e532325c78254433f86c893285ea4773
Utkarsh Kumar <31178132+Utkarsh9799@users.noreply.github.com>
2020-07-26 18:52:31 +0530

    Valid Perfect Square Solution

---
+++ ValidPerfectSquare.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        if(num == 1)
+            return true;
+        
+        int low = 1, high = num / 2;
+        while(low <= high)
+        {
+        
+            long long mid = (low + high) / 2;    
+            
+            if(mid * mid == num )
+                return true;
+            
+            if(mid < (num / mid))
+                low = mid + 1;
+            
+            else
+                high = mid - 1;
+        }
+        
+        return false;
+    }
+};
