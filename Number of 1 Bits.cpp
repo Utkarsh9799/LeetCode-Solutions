2d0dfdd3c02539f42e2f5e9104a100b87d92c053
Utkarsh Kumar <31178132+Utkarsh9799@users.noreply.github.com>
2020-08-16 23:35:20 +0530

    Solution Update

--- Number of 1 Bits.cpp
+++ Number of 1 Bits.cpp
@@ -4,8 +4,8 @@ public:
         int ctr = 0;
         while(n > 0)
         {
-            ctr += (n & 1);
-            n >>= 1;
+            n &= (n - 1);
+            ctr++;
         }
         return ctr;
     }
