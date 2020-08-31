5d2dc6cdab2216488d720d82e1b491caf16e0f48
Utkarsh Kumar <31178132+Utkarsh9799@users.noreply.github.com>
2020-08-31 23:25:54 +0530

    One pass solution update

--- SortColors.cpp
+++ SortColors.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int a = 0, b = 0, c = 0, n = nums.size();
-        for(int i = 0; i < n; i++)
+        int low = 0, mid = 0, high = nums.size() - 1;
+        while(mid <= high)
         {
-            if(nums[i] == 0)
-                a++;
-            if(nums[i] == 1)
-                b++;
-            if(nums[i] == 2)
-                c++;
-        }
-        for(int i = 0; i < a; i++)
-        {
-            nums[i] = 0;
-        }
-        for(int i = a; i < a + b; i++)
-        {
-            nums[i] = 1;
-        }
-        for(int i = a + b; i < a + b + c; i++)
-        {
-            nums[i] = 2;
+            if(nums[mid] == 0)
+            {
+                int temp = nums[low];
+                nums[low] = nums[mid];
+                nums[mid] = temp;
+                low++; mid++;
+            }
+            else if(nums[mid] == 1)
+                mid++;
+            else
+            {
+                int temp = nums[high];
+                nums[high] = nums[mid];
+                nums[mid] = temp;
+                high--;
+            }
         }
     }
 };
