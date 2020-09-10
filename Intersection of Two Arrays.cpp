7b4bf5c20e2d221220d55db77d56c3e6b0f7d2dd
Utkarsh Kumar <31178132+Utkarsh9799@users.noreply.github.com>
2020-09-10 17:15:26 +0530

    Optimised single set solution

--- Intersection of Two Arrays.cpp
+++ Intersection of Two Arrays.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
         int m = nums1.size(), n = nums2.size(), i;
         vector<int> intersection;
-        unordered_set<int> S, I;
+        unordered_set<int> S;
         for(i = 0; i < m; i++)
             S.insert(nums1[i]);
         
@@ -11,19 +11,8 @@ public:
         {
             if(S.find(nums2[i]) != S.end())
             {
-                if(I.empty())
-                {
-                    intersection.push_back(nums2[i]);
-                    I.insert(nums2[i]);
-                }
-                else
-                {
-                    if(I.find(nums2[i]) == I.end())
-                    {
-                        intersection.push_back(nums2[i]);
-                        I.insert(nums2[i]);
-                    }
-                }
+                intersection.push_back(nums2[i]);
+                S.erase(nums2[i]);
             }
         }
         return intersection;
