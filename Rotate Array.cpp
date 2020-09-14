cc3cc6d308dfc6a3444f0890fbfaadf2ca46845b
Utkarsh Kumar <31178132+Utkarsh9799@users.noreply.github.com>
2020-09-14 17:15:55 +0530

    Rotate Array constant space solution

--- Rotate Array.cpp
+++ Rotate Array.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        vector<int> ans(nums.size());
-        int i, n = nums.size();
-        for(i = 0; i < n; i++)
-            ans[(i + k) % n] = nums[i];
-        nums = ans;
+        if(k > nums.size())
+            k %= nums.size();
+        
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin() + k);
+        reverse(nums.begin() + k, nums.end());
     }
 };
