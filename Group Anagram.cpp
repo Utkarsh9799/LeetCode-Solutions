526ca6957d2319d2e680508a7e12b4ce5fc518a7
Utkarsh Kumar <31178132+Utkarsh9799@users.noreply.github.com>
2020-10-26 00:00:03 +0530

    Update solution

--- Group Anagram.cpp
+++ Group Anagram.cpp
@@ -1,9 +1,6 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        if(strs.size() == 1)
-            return {{strs[0]}};
-        
         vector<vector<string>> ans;
         unordered_map<string, vector<string>> M;
         for(int  i = 0; i < strs.size(); i++)
@@ -13,7 +10,9 @@ public:
             M[strs[i]].push_back(str);
         }
         for(auto i = M.begin(); i != M.end(); i++)
+        {
             ans.push_back(i -> second);
+        }
         return ans;
     }
 };
